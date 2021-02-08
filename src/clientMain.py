c2858b6028d67b7c03eb3c38a1338573e0b06cc3
Haydn Jones <haydnjonest@gmail.com>
2021-02-07 17:04:50 -0700

    Using servers command list

--- src/clientMain.py
+++ src/clientMain.py
@@ -10,7 +10,7 @@ from PyQt5.QtGui import *
 from Client.Client import *
 from Client.Calibration import *
 import numpy as np
-from Client.Command import COMMAND as cmd
+from Server.Command import COMMAND as cmd
 
 class MyWindow(QMainWindow, Ui_client):
 
